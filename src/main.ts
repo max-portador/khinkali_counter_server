15814acbf54084a76b5e397330b7a56e20543edc
max-portador <46213974+max-portador@users.noreply.github.com>
2022-09-17 21:11:38 +0300

    production cors settings

--- src/main.ts
+++ src/main.ts
@@ -11,7 +11,7 @@ async function start() {
       {
         cors: {
           credentials: true,
-          origin: "http://localhost:3000"
+          origin: '*'
         }
       });
     app.useGlobalPipes(new ValidationPipe());
